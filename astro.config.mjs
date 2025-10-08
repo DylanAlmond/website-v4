f062ebe1f86117bc1b011a8d5963a0a11513c6e8
Dylan Almond <dylan@dylanalmond.net>
2025-10-08 15:16:53 +0100

    Add mdx integration + custom image component for blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 import alpinejs from '@astrojs/alpinejs';
 import sitemap from '@astrojs/sitemap';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dylanalmond.net',
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [alpinejs(), sitemap()],
+  integrations: [alpinejs(), sitemap(), mdx()],
 });
