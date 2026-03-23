53160815a27ced3eb82c70ce87b4740c02131bcd
Dylan Almond <dylan@dylanalmond.net>
2026-03-23 14:51:45 +0000

    chore: update CV

--- src/config/site.ts
+++ src/config/site.ts
@@ -9,7 +9,7 @@ export const site = {
     github: 'https://github.com/dylanalmond',
     linkedin: 'https://linkedin.com/in/dylan-almond',
   },
-  cv: '/Dylan-Almond-CV-01-26-B.pdf',
+  cv: '/Dylan-Almond-CV-03-26.pdf',
   carbonCalculator: 'https://www.websitecarbon.com/website/dylanalmond-net/',
   services: [
     {
